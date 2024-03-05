93fa1bd13862d77f1c910d41720dfd78ae04320d
James O'Sullivan <james.a.osullivan01@gmail.com>
2024-03-05 17:04:06 +0000

    Started on first user story

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,27 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+@Service
 public class AccountService {
+
+   
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account save(Account account) {
+        return accountRepository.save(account); 
+}
+
+    public boolean findByUsername(String username){
+        
+        return false;
+    }
 }
